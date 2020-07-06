6806649a2a708d3ce1ec7c82b1812bf1e26a5f10
velann21 <velann21@gmail.com>
2020-07-06 18:51:41 +0200

    Complete Vue.js code for the project Quiz

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :numCorrect="numCorrect"
+            :numTottal="numTottal"/>
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox v-if="questions.length" 
+                       :currentQuestion="questions[index]"
+                       :next="next"
+                       :noQuestion="noQuestion"
+                       :increment="increment"/>
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data(){
+    return {
+      questions: [],
+      index: 0,
+      noQuestion: true,
+      numCorrect: 0,
+      numTottal: 0
+    }
+  },
+  methods: {
+    next(){
+      if((this.questions.length - 1) === this.index) {  this.noQuestion = false;  }
+      else { this.index++; }
+    },
+    increment(isCorrect){
+      if(isCorrect){
+        this.numCorrect++;
+      }
+      this.numTottal++;
+    }
+  },
+  mounted: function(){
+    fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple', {
+      method: 'get'
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonData) => {
+      this.questions = jsonData.results;
+    })
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled>Fancy Quiz App</b-nav-item>
+            <b-nav-item disabled>Counter: {{numCorrect}}/{{numTottal}}</b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTottal'
+    ]
+}
+</script>
